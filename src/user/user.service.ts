a22072acd85160907d44dc536a644ca79b5d08e1
Patrick Tavares <145362922+Trickqz@users.noreply.github.com>
2024-11-01 11:22:58 -0300

    feat users

---
+++ src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+}
